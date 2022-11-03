fce57d7b33baea5e9c9a6c0a9aa11873b6bff9b8
theanuragshukla <tachyonanurag@gmail.com>
2022-11-03 12:11:01 +0530

    added everything

---
+++ client/build/precache-manifest.8b8c6c715ff83e3e78c8d42146cd0e69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ccab06e71b52ff48e106b02775885fb",
+    "url": "/chesss/index.html"
+  },
+  {
+    "revision": "d57e2af4a83f9a0cc086",
+    "url": "/chesss/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "e6557af5ed72cfa20459",
+    "url": "/chesss/static/css/main.6b1a84b8.chunk.css"
+  },
+  {
+    "revision": "d57e2af4a83f9a0cc086",
+    "url": "/chesss/static/js/2.b359c947.chunk.js"
+  },
+  {
+    "revision": "193456205bf8fe965aa5bf3becc95d68",
+    "url": "/chesss/static/js/2.b359c947.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6557af5ed72cfa20459",
+    "url": "/chesss/static/js/main.975d0511.chunk.js"
+  },
+  {
+    "revision": "e869651d0c4b4c47a60f",
+    "url": "/chesss/static/js/runtime-main.a550f8ee.js"
+  }
+]);
